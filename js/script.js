85836a3c8442fdd6238fb84aae92c8a3d982f97d
Daniele <dmafrici84@gmail.com>
2020-07-12 17:23:10 +0200

    Lc js-simon correto. rifatto l'esercizio dopo la correzzione...

--- js/script.js
+++ js/script.js
@@ -6,18 +6,25 @@
 // creo un array con 5 numeri casuali univoci
 var num = 5;
 var arrayNumCasuli = [];
-for (var i = 0; i < num; i++) {
-  arrayNumCasuli.push(getRandomIntInclusive(1, 100));
-  for (var j = 0; (j < num) && (j != i); j++) {
-    if(arrayNumCasuli[i] == arrayNumCasuli[j]){
-      arrayNumCasuli.pop();
-      i--;
-    }
+// for (var i = 0; i < num; i++) {
+//   arrayNumCasuli.push(getRandomIntInclusive(1, 100));
+//   for (var j = 0; (j < num) && (j != i); j++) {
+//     if(arrayNumCasuli[i] == arrayNumCasuli[j]){
+//       arrayNumCasuli.pop();
+//       i--;
+//     }
+//   }
+// }
+
+while (arrayNumCasuli.length < num) {
+  var numRand = getRandomIntInclusive(1, 100);
+  if (!arrayNumCasuli.includes(numRand)) {
+    arrayNumCasuli.push(numRand);
   }
 }
 console.log("arrayNumCasuli",arrayNumCasuli);
 
-// Un alert i 5 numeri casuali univoci.
+// Un alert espone i 5 numeri casuali univoci.
 alert("Memorizza i seguenti 5 numeri: " + arrayNumCasuli + ". Appena ti senti pronto clicca ok");
 
 // Dopo 30 secondi l’utente deve inserire, un prompt alla volta, i numeri che ha visto precedentemente.
@@ -35,62 +42,76 @@ var interval = setInterval(function() {
 
     // chiedo all'utente di insrire, con un prompt alla volta,i numeri che ha visto precedentemente.
     var arrayNumUtente = [];
-    for (var i = 0; i < num; i++) {
-
+    while (arrayNumUtente.length < arrayNumCasuli.length) {
+      
       var numUtente = parseInt(prompt("Inserisci un numero"));
       console.log("numUtente",numUtente);
 
-      // verifico che il numero sia corretto
-      var numUtente1 = numeroValido(numUtente);
-      console.log("numUtetnte1",numUtente1);
-
-      // se è il munero è corretto
-      if (numUtente1) {
-
-        // inserisco il numero utente in un array
+      // verifico che l'utente inserisca un numero e che non lo abbia già scritto
+      // se le condizioni sono vere inserisco il numeo utente nell'array
+      if ((!arrayNumUtente.includes(numUtente)) && (!isNaN(numUtente))) {
         arrayNumUtente.push(numUtente);
-        console.log(arrayNumUtente[i]);
-
-        // verifico che l'utente non inserisca 2 o più volte lo stesso numero
-        for (var j = 0; (j < num) && (j != i); j++) {
-
-          if(arrayNumUtente[i] == arrayNumUtente[j]){
-            alert("attenzione il numero è stato già inserito");
-            arrayNumUtente.pop();
-            i--;
-          }
-
-        }
-        // se il numero non è corretto
+      } else if ((!isNaN(numUtente))) {
+        alert("attenzione il numero è stato già inserito");
       } else {
-
-      alert("Attenzione il valore inserito non è corretto");
-      i--;
-
+        alert("Attenzione il valore inserito non è corretto");
       }
-
     }
     console.log(arrayNumUtente);
 
+    // for (var i = 0; i < num; i++) {
+    //
+    //   var numUtente = parseInt(prompt("Inserisci un numero"));
+    //   console.log("numUtente",numUtente);
+    //
+    //   // verifico che il numero sia corretto
+    //   var numUtente1 = numeroValido(numUtente);
+    //   console.log("numUtetnte1",numUtente1);
+    //
+    //   // se è il munero è corretto
+    //   if (numUtente1) {
+    //
+    //     // inserisco il numero utente in un array
+    //     arrayNumUtente.push(numUtente);
+    //     console.log(arrayNumUtente[i]);
+    //
+    //     // verifico che l'utente non inserisca 2 o più volte lo stesso numero
+    //     for (var j = 0; (j < num) && (j != i); j++) {
+    //
+    //       if(arrayNumUtente[i] == arrayNumUtente[j]){
+    //         alert("attenzione il numero è stato già inserito");
+    //         arrayNumUtente.pop();
+    //         i--;
+    //       }
+    //
+    //     }
+    //     // se il numero non è corretto
+    //   } else {
+    //
+    //   alert("Attenzione il valore inserito non è corretto");
+    //   i--;
+    //
+    //   }
+    //
+    // }
+    // console.log(arrayNumUtente);
+
     //  il software dice quanti e quali dei numeri da indovinare sono stati individuati
     var numIndovinati  = [];
-    var punti = 0;
-    for (i = 0; i < arrayNumCasuli.length; i++) {
-      for (var j = 0; j < arrayNumUtente.length; j++) {
-        if (arrayNumCasuli[i] == arrayNumUtente[j] ) {
-          punti++;
-          numIndovinati.push(arrayNumUtente[j]);
-        }
+    for (var i = 0; i < arrayNumUtente.length; i++) {
+      if (arrayNumCasuli.includes(arrayNumUtente[i])) {
+        numIndovinati.push(arrayNumUtente[i]);
       }
     }
-    document.getElementById("numeri").innerHTML = "Hai indovinato " + punti + " numeri: " + numIndovinati;
-    console.log("punti",punti);
+    // for (i = 0; i < arrayNumCasuli.length; i++) {
+    //   for (var j = 0; j < arrayNumUtente.length; j++) {
+    //     if (arrayNumCasuli[i] == arrayNumUtente[j] ) {
+    //       numIndovinati.push(arrayNumUtente[j]);
+    //     }
+    //   }
+    // }
+    document.getElementById("numeri").innerHTML = "Hai indovinato " + numIndovinati.length + " numeri: " + numIndovinati;
     console.log(numIndovinati);
-
-    // se l'utente non indovina nessun numero
-    if (punti == 0) {
-      document.getElementById("numeri").innerHTML = "Mi dispiace non hai indovinato nessun numero";
-    }
 
   } else {
 
